31d1d7891f4be20cf9e3b258a928fafe481419dc
Timotej Čery <timo.cery1412@gmail.cokm>
2022-04-09 21:32:12 +0200

    Custom cell update

--- zadanieUI/ViewController.swift
+++ zadanieUI/ViewController.swift
@@ -7,8 +7,18 @@
 
 import UIKit
 
+struct WeatherCell{
+    let day : String
+    let temp:String
+    let feelsTemp: String
+    
+}
+
+
 class ViewController: UIViewController {
 
+    
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var temptLabel: UILabel!
@@ -17,14 +27,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var feelTemptLabel: UILabel!
     
-    var weatherDays: [String] {
-        ["Monday" , "Thuesday","Wednesday","Thursday","Friday","Saturday" , "Sunday"]
+    var weatherDays: [WeatherCell] {
+        [WeatherCell(day: "Pondelok", temp: "20", feelsTemp:"19"),
+         WeatherCell(day: "Utorok", temp: "15", feelsTemp:"19"),
+         WeatherCell(day: "Streda", temp: "18", feelsTemp:"19"),
+         WeatherCell(day: "Štvrtok", temp: "10", feelsTemp:"11"),
+         WeatherCell(day: "Piatok", temp: "9", feelsTemp:"8"),
+         WeatherCell(day: "Sobota", temp: "14", feelsTemp:"15"),
+         WeatherCell(day: "Nedela", temp: "25", feelsTemp:"25")
+        ]
     }
     
     override func viewDidLoad() {
         
         tableView.dataSource = self
-        tableView.reloadData()
+        tableView.register(UINib(nibName: "WeatherCustomeTableViewCell", bundle: nil ), forCellReuseIdentifier: "WeatherCustomeTableViewCell")
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -35,43 +52,32 @@ class ViewController: UIViewController {
 
 extension ViewController : UITableViewDataSource {
 //    POČET SEKCII V TABLE VIEW KED MAM POČET SEKCII 2 a v jednej sekcii mam 7 dni tak mi vráti 14 riadkov
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+    //    FUNKCIA KTORÁ VRÁTI POČET RIADKOV V TABLE VIEW
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherDays.count
+            //ZABEZPOECI ZOBRAZOVANIE IBA PRVÝCH 3 DNÍ
+            //        if section == 0 {
+            //            return 3
+            //        } else {
+            //            return weatherDays.count
+            //        }
+        }
 //    FUNKCIA KTORÁ NAFORMÁTUJE JEDNOTLIVE CELLS AK MOJA weatherCell NIE JE JE AKO WeatherTableViewCell TAK VRÁTIM PRÁZDNU TABULKU
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let weatherCell = tableView.dequeueReusableCell(withIdentifier: "WeatherTableViewCell", for: indexPath) as? WeatherTableViewCell else {
+        guard let weatherCell = tableView.dequeueReusableCell(withIdentifier: "WeatherCustomeTableViewCell", for: indexPath) as? WeatherCustomeTableViewCell else {
             return UITableViewCell()
         }
 //        let weatherCell  = tableView.dequeueReusableCell(withIdentifier: "WeatherTableViewCell", for: indexPath) as! WeatherTableViewCell
-        let day = weatherDays[indexPath.row]
-        weatherCell.weatherTextLabel.text = day
+        let weatherDay = weatherDays[indexPath.row]
+        weatherCell.dayLabel.text = weatherDay.day
+        weatherCell.tempLabel.text = weatherDay.temp
+        weatherCell.rainLabel.text = weatherDay.feelsTemp
+     
         return weatherCell
-    }
-    
-    
-//    FUNKCIA KTORÁ VRÁTI POČET RIADKOV V TABLE VIEW
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//ZABEZPOECI ZOBRAZOVANIE IBA PRVÝCH 3 DNÍ
-//        if section == 0 {
-//            return 3
-//        } else {
-//            return weatherDays.count
-//        }
-        return weatherDays.count
-
-    }
-    
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Header in \(section) section "
-    }
-    
-    
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return "Footer in \(section) section "
     }
 }
 
---
+++ zadanieUI/WeatherCustomeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCustomeTableViewCell.swift
+//  zadanieUI
+//
+//  Created by Timotej Čery on 09/04/2022.
+//
+
+import UIKit
+
+class WeatherCustomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+}
